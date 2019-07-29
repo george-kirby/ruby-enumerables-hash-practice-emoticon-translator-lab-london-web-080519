94092675830354652778f4be61f4a1401d7abf9c
George Kirby <georgeskirby@btinternet.com>
2019-07-29 17:43:20 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,19 @@
 # require modules here
+require "yaml"
 
-def load_library
+def load_library(file_path)
   # code goes here
+  key_only_hash = {
+    "get_meaning"=>{},
+    "get_emoticon"=>{}
+  }
+  emoticon_library = YAML.load_file(file_path).reduce(key_only_hash) do |memo, (key, value)|
+    
+    memo["get_meaning"][value[1]] = key
+    memo["get_emoticon"][value[0]] = value[1]
+    
+    memo
+  end
 end
 
 def get_japanese_emoticon
@@ -11,3 +23,5 @@ end
 def get_english_meaning
   # code goes here
 end
+
+p load_library('lib/emoticons.yml')
