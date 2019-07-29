0555d892af443e2c68df10c95bbcd5f3c0a8f9fc
George Kirby <georgeskirby@btinternet.com>
2019-07-29 17:55:18 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -19,7 +19,7 @@ end
 
 def get_japanese_emoticon(file_path, english_emoticon)
   # code goes here
-  load_library(file_path)
+  emoticon_library = load_library(file_path)
   
   if emoticon_library[english_emoticon]
     japanese_emoticon = emoticon_library[english_emoticon]
